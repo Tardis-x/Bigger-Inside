a107b7175a6f5f3e4a9ac47118eb61f9e54fbc5a
XaycMD <vpetyushka@gmail.com>
2018-07-05 16:21:18 +0300

    Add loading indicator to SignInScene

---
+++ Assets/Scripts/MenuScene/UI/Logout.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using Firebase.Auth;
+using Google;
+using UnityEngine.SceneManagement;
+
+public class Logout : MonoBehaviour {
+
+  //---------------------------------------------------------------------
+  // Internal
+  //---------------------------------------------------------------------
+
+  public void OnLogout()
+  {
+    FirebaseAuth.DefaultInstance.SignOut();
+    GoogleSignIn.DefaultInstance.SignOut();
+    SceneManager.LoadScene("SignInScene");
+  }
+}
