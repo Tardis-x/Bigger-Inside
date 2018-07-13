bb275e7532d0d400be60568c5efd610e1cb86274
hubal.oleg <hubal.oleg@gmal.com>
2018-07-13 17:40:18 +0300

    Revert "Update sign in scene"
    This reverts commit 6c2e9bb

--- Assets/Scripts/SignInScene/Fade.cs
+++ Assets/Scripts/SignInScene/Fade.cs
@@ -1,4 +1,7 @@
-﻿using UnityEngine;
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Analytics;
 using UnityEngine.UI;
 
 public class Fade : MonoBehaviour
---
+++ Assets/Scripts/SignInScene/LoadingCircle.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LoadingCircle : MonoBehaviour {
+
+	//---------------------------------------------------------------------
+	// Editor
+	//---------------------------------------------------------------------
+	
+	[SerializeField]
+	private float _rotateSpeed = 200f;
+
+	[SerializeField] private GameObject _parent;
+	
+	//---------------------------------------------------------------------
+	// Internal
+	//---------------------------------------------------------------------
+	
+	private RectTransform _rectComponent;
+	
+	//---------------------------------------------------------------------
+	// Messages
+	//---------------------------------------------------------------------
+	
+	// Use this for initialization
+	void Start () 
+	{
+		_rectComponent = GetComponent<RectTransform>();
+	}
+	
+	// Update is called once per frame
+	void Update()
+	{
+		_rectComponent.Rotate(0f, 0f, -_rotateSpeed * Time.deltaTime);
+	}
+	
+	//---------------------------------------------------------------------
+	// Helpers
+	//---------------------------------------------------------------------
+
+	public void Show()
+	{
+		_parent.SetActive(true);
+	}
+}
