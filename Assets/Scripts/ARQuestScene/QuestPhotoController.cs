12d29189813680e521c5af9594574349067e47b7
Taras Leskiv <leskiv.taras@gmail.com>
2018-07-26 12:39:55 +0300

    Test Commit of iOS functional implementation

--- Assets/Scripts/ARQuestScene/QuestPhotoController.cs
+++ Assets/Scripts/ARQuestScene/QuestPhotoController.cs
@@ -9,24 +9,26 @@ using DeadMosquito.AndroidGoodies.Internal;
 using DeadMosquito.IosGoodies;
 using Firebase.Auth;
 using Firebase.Storage;
+using Unity.Collections;
 using UnityEngine.UI;
 
 public class QuestPhotoController : MonoBehaviour
 {
     public Image photoTextureHolder;
     private Texture2D tex;
-    private string userID;
+    private string userID = "1";
     private FirebaseStorage storage;
     public string picturePath;
     private int imageSize = 0;
     private StorageReference pictureReference;
     QuestManager _questManager;
     public string imageUrl;
+    byte[] imageBytes;
+    string pictureNameInStorage;
 
     void Awake()
     {
         Debug.Log("QuestPhotoController.Awake");
-
         // obtain reference to object that represents quest manager
         QuestManagerReferenceInitialization();
     }
@@ -79,33 +81,68 @@ public class QuestPhotoController : MonoBehaviour
                 false);
         }
 #elif UNITY_IOS
-// IOS STUFF
         Debug.Log("Take a picture button Clicked.");
         const bool allowEditing = false;
-        const float compressionQuality = 0.7f;
+        const float compressionQuality = 0.1f;
         const IGImagePicker.CameraType cameraType = IGImagePicker.CameraType.Rear;
         const IGImagePicker.CameraFlashMode flashMode = IGImagePicker.CameraFlashMode.Off;
         IGImagePicker.PickImageFromCamera(texture =>
             {
                 Debug.Log("Successfully picked image from camera");
+                tex = texture;
+                Debug.Log("Texture saved.");
                 photoTextureHolder.sprite = SpriteFromTex2D(texture);
+                Debug.Log("Texture placed on image holder.");
+//                IGImagePicker.SaveImageToGallery(texture);
+//                Debug.Log("Image saved to gallery.");
+                //Creating local file from texture
+                picturePath = Application.persistentDataPath + "/photo.jpeg";
+                if (File.Exists(picturePath))
+                {
+                    File.Delete(picturePath);
+                    Debug.Log("File was deleted.");
+                }
+                File.WriteAllBytes(picturePath, texture.EncodeToJPG());
+                Debug.Log("File was created.");
+                if (File.Exists(picturePath))
+                {
+                    Debug.Log("It is there, man...");
+                    //Upload
+                    OnUploadButtonClick();
+                }
             }, 
             () => Debug.Log("Picking image from camera cancelled"), 
             compressionQuality,
         allowEditing, cameraType, flashMode);
+        
 #endif
     }
     
    public void OnSharePictureButtonClick()
     {
-#if UNITY_ANDROID
         Debug.Log("Share Button Clicked.");
+#if UNITY_ANDROID
         tex = photoTextureHolder.mainTexture as Texture2D;
         string subject = "DevFest Photo";
         string body = "My photo with speaker!";
         AGShare.ShareTextWithImage(subject, body, tex, true, "Share via facebook...");
 #elif UNITY_IOS
-        
+        var screenPosition = new Vector2(Screen.width / 2, Screen.height / 2);
+
+        IGShare.Share(
+            activityType =>
+            {
+                if (string.IsNullOrEmpty(activityType))
+                {
+                    Debug.Log("Posting was canceled or unknown result");
+                }
+                else
+                {
+                    Debug.Log("DONE sharing, activity: " + activityType);
+                }
+            },
+            error => Debug.LogError("Error happened when sharing activity: " + error),
+            "Google DevFest!", tex, screenPosition);
 #endif
     }
 
@@ -140,7 +177,51 @@ public class QuestPhotoController : MonoBehaviour
                 }
             });
 #elif UNITY_IOS
-        
+        picturePath = "file://" + picturePath;
+        Debug.Log("Picture path changed successfully.");
+        try
+        {
+            userID = FirebaseAuth.DefaultInstance.CurrentUser.UserId;
+            Debug.Log("UserID string was initialized.");
+        }
+        catch (Exception e)
+        {
+            Debug.Log(e.Message);
+        }
+        userID = userID.Replace(" ", string.Empty);
+        Debug.Log("String was changed.");
+        //Creating reference to a picture in the database
+        storage = FirebaseStorage.DefaultInstance;
+        Debug.Log("Firebase storage was initialized.");
+        StorageReference storageRef = storage.GetReferenceFromUrl("gs://hoverboard-v2-dev.appspot.com");
+        Debug.Log("Storage reference was set.");
+        pictureNameInStorage = "/Pictures/" + userID + ".jpeg";
+        Debug.Log("Path in storage was set.");
+        pictureReference = storageRef.Child(pictureNameInStorage);
+        Debug.Log("Child reference was set.");
+        //Uploading image
+        var pictureMetadata = new MetadataChange();
+        pictureMetadata.ContentType = "image/jpeg";
+        Debug.Log("Metadata was initialized.");
+        var task = pictureReference.PutFileAsync(picturePath, pictureMetadata);
+        Debug.Log("Image started uploading.");
+        task.ContinueWith(resultTask =>
+        {
+            imageUrl = "gs://hoverboard-v2-dev.appspot.com" + pictureNameInStorage;
+            Debug.Log("Image URL was set.");
+            _questManager.questProgress.photoData.imgUrl = imageUrl;
+            _questManager.CheckInPhoto(this);
+            Debug.Log("Image URL was changed in FireBase.");
+            if (!resultTask.IsFaulted && !resultTask.IsCanceled)
+            {
+                Debug.Log("Upload finished.");
+            }
+            else
+            {
+                Debug.Log(resultTask.Exception.Message);
+            }
+        });
+
 #endif
     }
 #if UNITY_ANDROID
@@ -154,6 +235,12 @@ public class QuestPhotoController : MonoBehaviour
         // Dismiss spinner after all the background work is done
         spinner.Dismiss();
 
+    }
+#elif UNITY_IOS
+    private IEnumerator UploadFIleAfterThreeSeconds()
+    {
+        yield return new WaitForSeconds(10);
+        
     }
 #endif
     Sprite SpriteFromTex2D(Texture2D texture)
