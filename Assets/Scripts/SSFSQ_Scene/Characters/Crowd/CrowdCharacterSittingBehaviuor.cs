e982058d03299d0e4d56e14b4ad23ad8c1ccf2e4
HouseMD <jamesHetfield0>
2018-07-30 19:38:24 +0300

    Fix crowd stand up and throw animation

---
+++ Assets/Scripts/SSFSQ_Scene/Characters/Crowd/CrowdCharacterSittingBehaviuor.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace ua.org.gdg.devfest
+{
+  public class CrowdCharacterSittingBehaviuor : StateMachineBehaviour
+  {
+    // OnStateEnter is called when a transition starts and the state machine starts to evaluate this state
+    override public void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+      animator.SetBool("Sitting", true);
+    }
+
+    // OnStateExit is called when a transition ends and the state machine finishes evaluating this state
+    override public void OnStateExit(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+      animator.SetBool("Sitting", false);
+    }
+  }
+}
