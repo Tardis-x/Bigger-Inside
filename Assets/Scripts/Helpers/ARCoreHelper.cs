a579fa57976aea694fad626edc6f2e24a5e9fb53
Nikita Nesterchuk <nik.nesterchuk@gmail.com>
2018-08-05 13:50:26 +0300

    Testing ARCore support checking

---
+++ Assets/Scripts/Helpers/ARCoreHelper.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+
+namespace ua.org.gdg.devfest {
+    #if UNITY_ANDROID
+    public class ARCoreHelper: MonoBehaviour {
+        public static AndroidJavaObject GetAppContext() {
+            var unityPlayer = new AndroidJavaClass("com.unity3d.player.UnityPlayer");
+            var activity = unityPlayer.GetStatic<AndroidJavaObject>("currentActivity");
+            var context = activity.Call<AndroidJavaObject>("getApplicationContext");
+            return context;
+        }
+
+        public static bool ArCoreCheck() {
+            var arCoreApk = new AndroidJavaClass("com.google.ar.core.ArCoreApk");
+            var arCoreApkInstance = arCoreApk.CallStatic<AndroidJavaObject>("getInstance");
+            var availability = arCoreApkInstance.Call<AndroidJavaObject>("checkAvailability", GetAppContext());
+
+            var isUnsupported = availability.Call<bool>("isUnsupported");
+            return !isUnsupported;
+        }
+    }
+    #endif
+}
---
+++ Assets/TestManager.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using ua.org.gdg.devfest;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TestManager : MonoBehaviour {
+	public Text Text;
+	// Use this for initialization
+	void Start () {
+		#if UNITY_ANDROID
+		var isArCoreSupported = ARCoreHelper.ArCoreCheck();
+		Text.text = isArCoreSupported.ToString();
+		#endif
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
