55440a0b404e35fdc342b199d7a109ad6983a024
Nikita Nesterchuk <nik.nesterchuk@gmail.com>
2018-08-07 18:44:22 +0300

    Throws tomatoes on events. Fixed sneaker position and animation

--- Assets/Scripts/SSFSQ_Scene/Characters/Crowd/TomatoThrowBehaviour.cs
+++ Assets/Scripts/SSFSQ_Scene/Characters/Crowd/TomatoThrowBehaviour.cs
@@ -4,16 +4,20 @@ namespace ua.org.gdg.devfest
 {
   public class TomatoThrowBehaviour : StateMachineBehaviour
   {
+    [Header("Events")]
+    [SerializeField] private GameEvent _onStartTomatoesThrowing;
+    [SerializeField] private GameEvent _onStopTomatoesThrowing;
+    
     // OnStateEnter is called when a transition starts and the state machine starts to evaluate this state
     override public void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
     {
-      AnimationManager.Instance.TurnTomatoesOn();
+      _onStartTomatoesThrowing.Raise();
     }
     
     // OnStateEnter is called when a transition starts and the state machine starts to evaluate this state
     override public void OnStateExit(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
     {
-      AnimationManager.Instance.TurnTomatoesOff();
+      _onStopTomatoesThrowing.Raise();
     }
   }
 }
