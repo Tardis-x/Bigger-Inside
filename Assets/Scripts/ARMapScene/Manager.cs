a0fb7d6070fc229fcc0fc2865102f013643e7a86
Nikita Nesterchuk <nik.nesterchuk@gmail.com>
2018-08-13 12:38:41 +0300

    Preparations in AR Map Scene

---
+++ Assets/Scripts/ARMapScene/Manager.cs
@@ -0,0 +1,52 @@
+﻿using UnityEngine;
+using Vuforia;
+
+namespace ua.org.gdg.devfest
+{
+	public class Manager : MonoBehaviour
+	{
+		//---------------------------------------------------------------------
+		// Editor
+		//---------------------------------------------------------------------
+
+		[Header("Prefabs")]
+		[SerializeField] private GameObject _environment;
+
+		[Space]
+		[Header("Targets")]
+		[SerializeField] private GameObject _imageTarget;
+		[SerializeField] private GameObject _planeFinder;
+		
+		//---------------------------------------------------------------------
+		// Messages
+		//---------------------------------------------------------------------
+
+		private void Start()
+		{
+			var arCoreSupport = ARCoreHelper.CheckArCoreSupport();
+			PrepareScene(arCoreSupport);
+		}
+
+		public void OnContentPlaced(GameObject environment)
+		{
+			_planeFinder.SetActive(false);
+			_planeFinder.GetComponent<PlaneFinderBehaviour>().OnInteractiveHitTest.RemoveAllListeners();
+		}
+		
+		//---------------------------------------------------------------------
+		// Internal
+		//---------------------------------------------------------------------
+		
+		private void PrepareScene(bool arCoreSupport)
+		{
+			_planeFinder.gameObject.SetActive(arCoreSupport);
+      
+			_imageTarget.SetActive(!arCoreSupport);
+      
+			if (!arCoreSupport)
+			{
+				Instantiate(_environment, _imageTarget.transform);        
+			}
+		}
+	}
+}
