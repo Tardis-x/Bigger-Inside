d3e8a85aedd0939298c86187284f2ec44c7d20f7
HouseMD <jamesHetfield0>
2018-08-14 17:57:23 +0300

    Add Enemy ScriptableObject

---
+++ Assets/Scripts/Variables/IntReference.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace ua.org.gdg.devfest
+{
+	[Serializable]
+	public class IntReference
+	{
+		public bool UseConstant = true;
+		public int ConstantValue;
+		public IntVariable Variable;
+
+		public IntReference()
+		{ }
+
+		public IntReference(int value)
+		{
+			UseConstant = true;
+			ConstantValue = value;
+		}
+
+		public int Value
+		{
+			get { return UseConstant ? ConstantValue : Variable.RuntimeValue; }
+		}
+
+		public static implicit operator int(IntReference reference)
+		{
+			return reference.Value;
+		}
+	}
+}
