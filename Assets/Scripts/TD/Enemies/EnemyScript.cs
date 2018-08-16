d711d7af127e9fdf7d6b06348d75df4dfd788394
HouseMD <jamesHetfield0>
2018-08-16 12:04:53 +0300

    Add GameManager, Enemy level

--- Assets/Scripts/TD/Enemies/EnemyScript.cs
+++ Assets/Scripts/TD/Enemies/EnemyScript.cs
@@ -11,13 +11,38 @@ namespace ua.org.gdg.devfest
     [SerializeField] private Enemy _enemy;
     
     //---------------------------------------------------------------------
-    // Public
+    // Internal
     //---------------------------------------------------------------------
 
-    public void SetLevel(int level)
+    private int _hp;
+    private int _money;
+    
+    //---------------------------------------------------------------------
+    // Messages
+    //---------------------------------------------------------------------
+
+    private void Awake()
     {
-      _enemy.HP.Value += _enemy.HPPerLevel * level;
-      _enemy.Money.Value += _enemy.MoneyPerLevel * level;
+      _hp = _enemy.HP.Value;
+      _money = _enemy.Money.Value;
+    }
+    
+    //---------------------------------------------------------------------
+    // Public
+    //---------------------------------------------------------------------
+    
+    public void LevelUp()
+    {
+      _hp += _enemy.HPPerLevel;
+      _money += _enemy.MoneyPerLevel;
+    }
+
+    public EnemyScript GetInstance(int level, Transform parent)
+    {
+      var instance = Instantiate(this, parent);
+      instance._hp += _enemy.HPPerLevel.Value * level;
+      instance._money += _enemy.MoneyPerLevel.Value * level;
+      return instance;
     }
   }
 }
