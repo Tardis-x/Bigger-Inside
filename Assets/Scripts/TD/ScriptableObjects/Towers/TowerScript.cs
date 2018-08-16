7594912214d2bde7ec38300cb7f52adbba8ac15f
HouseMD <jamesHetfield0>
2018-08-16 13:08:50 +0300

    Add Tower Enemy Tracking

--- Assets/Scripts/TD/ScriptableObjects/Towers/TowerScript.cs
+++ Assets/Scripts/TD/ScriptableObjects/Towers/TowerScript.cs
@@ -1,22 +1,64 @@
-﻿using System.Collections;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using UnityEngine;
 
 namespace ua.org.gdg.devfest
 {
 	public class TowerScript : MonoBehaviour
 	{
+		//---------------------------------------------------------------------
+		// Editor
+		//---------------------------------------------------------------------
+		
+		[SerializeField] private Tower _tower;
+		
+		//---------------------------------------------------------------------
+		// Messages
+		//---------------------------------------------------------------------
 
-		// Use this for initialization
-		void Start()
+		private void OnTriggerEnter(Collider other)
 		{
-
+			// Get enemy script
+			var enemy = other.GetComponent<EnemyScript>();
+			
+			// If it's enemy
+			if (enemy != null)
+			{
+				// It's got in range
+				_targetsInRange.Add(enemy);
+				
+				// If no targets aquired aquire enemy
+				if (_target == null) _target = enemy;
+			}
 		}
 
-		// Update is called once per frame
-		void Update()
+		private void OnTriggerExit(Collider other)
 		{
+			// Get enemy script
+			var enemy = other.GetComponent<EnemyScript>();
+			
+			// If it's enemy
+			if (enemy != null)
+			{
+				// It's got out of range
+				_targetsInRange.Remove(enemy);
+				
+				// If current target left the range need to aquire anuther one
+				if(enemy == _target) AquireNewTarget();
+			}
+		}
 
+		//---------------------------------------------------------------------
+		// Internal
+		//---------------------------------------------------------------------
+
+		private EnemyScript _target;
+		private List<EnemyScript> _targetsInRange = new List<EnemyScript>();
+
+		private void AquireNewTarget()
+		{
+			// If there are enemies in range shoot first to enter
+			// If not - no target can be aquired
+			_target = _targetsInRange.Count > 0 ? _targetsInRange[0] : null;
 		}
 	}
 }
