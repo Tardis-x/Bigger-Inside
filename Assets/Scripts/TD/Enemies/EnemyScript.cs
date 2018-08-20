f5d84a12ea5e0a525cabb67fa3f4a5a83d199465
HouseMD <jamesHetfield0>
2018-08-20 14:21:54 +0300

    Add Enemy Damage Taking based on Resistances

--- Assets/Scripts/TD/Enemies/EnemyScript.cs
+++ Assets/Scripts/TD/Enemies/EnemyScript.cs
@@ -1,5 +1,5 @@
-﻿using UnityEngine;
-using UnityEngine.AI;
+﻿using System.Linq;
+using UnityEngine;
 
 namespace ua.org.gdg.devfest
 {
@@ -28,6 +28,11 @@ namespace ua.org.gdg.devfest
       _dieEvent.Raise(gameObject);
       gameObject.layer = DEAD_ENEMIES_LAYER;
       _agent.Die();
+    }
+
+    private float CalculateDamageCoefficient(float resist)
+    {
+      return 1 - resist * .01f;
     }
 
     //---------------------------------------------------------------------
@@ -62,7 +67,18 @@ namespace ua.org.gdg.devfest
 
     public void GetShot(Projectile projectile)
     {
-      HP -= (int) Mathf.Round(projectile.Damage);
+      float dmg = projectile.Damage;
+      var resist = _enemy.Resistances.ResistancesList.FirstOrDefault(x => x.Type == projectile.Type);
+      
+      if (resist != null)
+      {
+        dmg *= CalculateDamageCoefficient(resist.Amount);
+      }
+      
+      HP -= (int) Mathf.Round(dmg);
+      
+      Debug.Log(gameObject.name + " taken " + dmg + " damage from " + projectile.Type + " projectile");
+      
       if (HP <= 0) Die();
     }
 
@@ -73,5 +89,6 @@ namespace ua.org.gdg.devfest
     public int HP { get; private set; }
 
     public int Money { get; private set; }
+    
   }
 }
