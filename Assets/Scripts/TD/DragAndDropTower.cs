80aca9357e34192cf275b4550ed2d98a389c5f63
Nikita Nesterchuk <nik.nesterchuk@gmail.com>
2018-08-20 17:38:20 +0300

    Started drag and drop task

---
+++ Assets/Scripts/TD/DragAndDropTower.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+
+namespace ua.org.gdg.devfest
+{
+	public class DragAndDropTower : MonoBehaviour
+	{
+		private void Update()
+		{
+			if (Input.touchCount > 0)
+			{
+				var touch = Input.GetTouch(0);
+
+				if (touch.phase == TouchPhase.Stationary || touch.phase == TouchPhase.Moved)
+				{
+					var touchedPos =
+						Camera.main.ScreenToWorldPoint(new Vector3(touch.position.x, touch.position.y, 10));					
+					var newPos = Vector3.Lerp(transform.position, touchedPos, Time.deltaTime * 10);
+					
+					transform.position = new Vector3(newPos.x, transform.position.y, newPos.z);
+				}
+			}
+		}
+	}
+}
