8ddb0b90fb032d1acb5a26741326e5ebd9e1314a
HouseMD <jamesHetfield0>
2018-08-26 17:59:27 +0300

    Add Towers and Enemy tutorial

---
+++ Assets/Scripts/TD/UI/EnemyTutorialScript.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+namespace ua.org.gdg.devfest
+{
+  public class EnemyTutorialScript : MonoBehaviour
+  {
+    [SerializeField] private Image _happyImage;
+    
+    public void EnableHappyIcon(int value)
+    {
+      _happyImage.gameObject.SetActive(value == 1);
+    }
+  }
+}
