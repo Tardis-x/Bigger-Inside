6c421a83b9ee303ba6ae11700c071e392122d9e4
Nikita Nesterchuk <nik.nesterchuk@gmail.com>
2018-08-27 15:07:52 +0300

    Throwing coins when happy enemy disappears

---
+++ Assets/UIEventListener.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+namespace ua.org.gdg.devfest
+{
+    public class UIEventListener : MonoBehaviour
+    {
+
+        //---------------------------------------------------------------------
+        // Editor
+        //---------------------------------------------------------------------
+
+        [Header("Text")] 
+        [SerializeField] private Text _moneyText;
+        [SerializeField] private Text _scoreText;
+        [SerializeField] private Text _enemiesLeftText;
+
+        //---------------------------------------------------------------------
+        // Events
+        //---------------------------------------------------------------------
+
+        public void OnCreepDisappeared(GameObject creep)
+        {
+            var creepEnemyScript = creep.GetComponent<EnemyScript>();
+
+            if (creepEnemyScript.Happy)
+            {
+                var score = int.Parse(_scoreText.text) + 1;
+                var money = int.Parse(_moneyText.text) + creepEnemyScript.Money;
+                
+                _scoreText.text = score.ToString();
+                _moneyText.text = money.ToString();
+            }
+            else
+            {
+                var enemiesLeft = int.Parse(_enemiesLeftText.text) - 1;
+                _enemiesLeftText.text = enemiesLeft.ToString();
+            }
+        }
+    }
+}
