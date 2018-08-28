4ade995b065baff9e5dcb87adfe56b77f1654763
Nikita Nesterchuk <nik.nesterchuk@gmail.com>
2018-08-28 15:16:42 +0300

    Added play button with start event

---
+++ Assets/Scripts/TD/UI/StatsPanel.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+namespace ua.org.gdg.devfest
+{
+	public class StatsPanel : MonoBehaviour
+	{
+		//---------------------------------------------------------------------
+		// Editor
+		//---------------------------------------------------------------------
+
+		[SerializeField] private Text _text;
+		
+		//---------------------------------------------------------------------
+		// Public
+		//---------------------------------------------------------------------
+
+		public void SetText(string text)
+		{
+			_text.text = text;
+		}
+
+		public void SetActive(bool value)
+		{
+			gameObject.SetActive(value);
+		}
+	}
+}
---
+++ Assets/Scripts/TD/UI/UIEventListener.cs
@@ -0,0 +1,76 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+namespace ua.org.gdg.devfest
+{
+  public class UIEventListener : MonoBehaviour
+  {
+    //---------------------------------------------------------------------
+    // Editor
+    //---------------------------------------------------------------------
+
+    [Header("Stats panels")] 
+    [SerializeField] private StatsPanel _moneyPanel;
+    [SerializeField] private StatsPanel _scorePanel;
+    [SerializeField] private StatsPanel _enemiesLeftPanel;
+
+    [Space] 
+    [Header("Panels")] 
+    [SerializeField] private TowerUpgradePanelScript _upgradePanel;
+    [SerializeField] private RectTransform _towerPanel;
+    [SerializeField] private HallUnlockPanelScript _hallUnlockPanel;
+
+    [Space]
+    [Header("Buttons")]
+    [SerializeField] private Button _playButton;
+    
+    [Space]
+    [Header("Public Variables")]
+    [SerializeField] private IntReference _money;
+    [SerializeField] private IntReference _score;
+    [SerializeField] private IntReference _enemiesLeft;
+
+    //---------------------------------------------------------------------
+    // Events
+    //---------------------------------------------------------------------
+
+    public void OnStart()
+    {
+      _moneyPanel.SetActive(true);
+      _scorePanel.SetActive(true);
+      _enemiesLeftPanel.SetActive(true);
+      _towerPanel.gameObject.SetActive(true);
+      _playButton.gameObject.SetActive(false);
+    }
+
+    public void OnTowerSelected(GameObject tower)
+    {
+      var towerScript = tower.GetComponent<TowerScript>();
+      _upgradePanel.SelectedTower = towerScript;
+      _upgradePanel.SetUpgradeCostText(towerScript.UpgradeCost);
+      _upgradePanel.SetSellCostText(towerScript.SellCost);
+      
+      _towerPanel.gameObject.SetActive(false);
+      _upgradePanel.gameObject.SetActive(true);
+    }
+
+    public void OnTowerDeselected()
+    {
+      _towerPanel.gameObject.SetActive(true);
+      _upgradePanel.gameObject.SetActive(false);
+    }
+
+    public void OnUIUpdateRequested()
+    {
+      _moneyPanel.SetText(_money.Value.ToString());
+      _scorePanel.SetText(_score.Value.ToString());
+      _enemiesLeftPanel.SetText(_enemiesLeft.Value.ToString());
+    }
+    
+    public void OnHallUnlocked(int hallNumber)
+    {
+      _hallUnlockPanel.gameObject.SetActive(true);
+      _hallUnlockPanel.OnHallUnlocked(hallNumber);
+    }
+  }
+}
