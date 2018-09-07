0cc369ee21409316dff8d3dedfb049cf9d8fe2a9
HouseMD <jamesHetfield0>
2018-09-07 17:07:53 +0300

    Delete UserAvatar images caching

--- Assets/Scripts/MenuScene/UI/UserAvatarManager.cs
+++ Assets/Scripts/MenuScene/UI/UserAvatarManager.cs
@@ -1,7 +1,4 @@
-﻿using System;
-using System.Collections;
-using Facebook.Unity;
-using UnityEngine;
+﻿using UnityEngine;
 using UnityEngine.UI;
 using Firebase.Auth;
 
@@ -38,19 +35,8 @@ namespace ua.org.gdg.devfest
 		public void SetUserAvatar()
 		{
 			if (FirebaseAuth.DefaultInstance.CurrentUser == null) return;
-
-			if(FB.IsLoggedIn) FB.API("/me/picture?redirect=false", HttpMethod.GET, ProfilePhotoCallback);
-			else _imageLoader.LoadImage(FirebaseAuth.DefaultInstance.CurrentUser.PhotoUrl.OriginalString, _userAvatar);
-		}
-		
-		private void ProfilePhotoCallback (IGraphResult result)
-		{
-			if (String.IsNullOrEmpty(result.Error) && !result.Cancelled) {
-				IDictionary data = result.ResultDictionary["data"] as IDictionary;
-				string photoURL = data["url"] as String;
- 
-				_imageLoader.LoadImage(photoURL, _userAvatar);
-			}
+			
+			_imageLoader.LoadImage(FirebaseAuth.DefaultInstance.CurrentUser.PhotoUrl.OriginalString, _userAvatar);
 		}
 	}
 }
