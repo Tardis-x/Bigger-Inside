e8d1ad036b31df11effd1514128e9429b1425ea9
HouseMD <jamesHetfield0>
2018-09-07 16:43:31 +0300

    Fix UserAvatar bug with Facebook SignIn

--- Assets/Scripts/MenuScene/UI/UserAvatarManager.cs
+++ Assets/Scripts/MenuScene/UI/UserAvatarManager.cs
@@ -1,4 +1,7 @@
-﻿using UnityEngine;
+﻿using System;
+using System.Collections;
+using Facebook.Unity;
+using UnityEngine;
 using UnityEngine.UI;
 using Firebase.Auth;
 
@@ -36,7 +39,18 @@ namespace ua.org.gdg.devfest
 		{
 			if (FirebaseAuth.DefaultInstance.CurrentUser == null) return;
 
-			_imageLoader.LoadImage(FirebaseAuth.DefaultInstance.CurrentUser.PhotoUrl.OriginalString, _userAvatar);
+			if(FB.IsLoggedIn) FB.API("/me/picture?redirect=false", HttpMethod.GET, ProfilePhotoCallback);
+			else _imageLoader.LoadImage(FirebaseAuth.DefaultInstance.CurrentUser.PhotoUrl.OriginalString, _userAvatar);
+		}
+		
+		private void ProfilePhotoCallback (IGraphResult result)
+		{
+			if (String.IsNullOrEmpty(result.Error) && !result.Cancelled) {
+				IDictionary data = result.ResultDictionary["data"] as IDictionary;
+				string photoURL = data["url"] as String;
+ 
+				_imageLoader.LoadImage(photoURL, _userAvatar);
+			}
 		}
 	}
 }
