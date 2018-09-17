09f93660e6a6caef79e0af012d9b9498b7f0dfc9
Hubal Oleg <hubal.oleg@gmail.com>
2018-09-17 15:48:15 +0300

    Refactor

--- Assets/Scripts/Helpers/Singleton.cs
+++ Assets/Scripts/Helpers/Singleton.cs
@@ -1,13 +1,3 @@
-/*
- * Copyright (c) 2016-2017, Ivan Gadzhega.
- * All rights reserved.
- *
- * This software is the confidential and proprietary information of Ivan Gadzhega.
- * ("Confidential Information"). You shall not disclose such Confidential Information
- * and shall use it only in accordance with the terms of the license agreement
- * you entered into with Ivan Gadzhega.
- */
-
 using System.Diagnostics.CodeAnalysis;
 using UnityEngine;
 
