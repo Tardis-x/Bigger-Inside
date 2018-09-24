91bf929943e8c5432012576a3afa468e2ac2992d
Andriy Marshalek <amarshalek@ninevastudios.com>
2018-09-24 15:17:48 +0300

    Minor fix

--- Assets/Scripts/ARQuestScene/QuestManager.cs
+++ Assets/Scripts/ARQuestScene/QuestManager.cs
@@ -29,7 +29,7 @@ public class QuestManager : MonoBehaviour
 	void Awake()
 	{
 		// Set up the Editor before calling into the realtime database.
-		FirebaseApp.DefaultInstance.SetEditorDatabaseUrl("https://hoverboard-firebase.firebaseio.com/");
+		FirebaseApp.DefaultInstance.SetEditorDatabaseUrl("https://hoverboard-firebase.firebaseio.com/Quest");
 		Debug.Log("QuestManager.Awake.URL");
 		// obtain reference to object that represents quest UI
 		UiReferenceInitialization();
