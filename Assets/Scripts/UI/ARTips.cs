c4f0f79b550bff42764fcba5681fefa4344533a0
Hubal Oleg <hubal.oleg@gmail.com>
2018-10-08 13:41:59 +0300

    TD Game: AR Core - fix vuforia tips

--- Assets/Scripts/UI/ARTips.cs
+++ Assets/Scripts/UI/ARTips.cs
@@ -1,7 +1,4 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-using UnityEngine.UI;
+﻿using UnityEngine;
 
 namespace ua.org.gdg.devfest
 {
@@ -23,10 +20,14 @@ namespace ua.org.gdg.devfest
 		//---------------------------------------------------------------------
 		// Messages
 		//---------------------------------------------------------------------
-		
+
+		private void Awake()
+		{
+			_arCoreSupport = ARCoreHelper.CheckArCoreSupport();			
+		}
+
 		private void Start()
 		{
-			_arCoreSupport = ARCoreHelper.CheckArCoreSupport();
 			ShowTipPanel(true);
 		}
 		
