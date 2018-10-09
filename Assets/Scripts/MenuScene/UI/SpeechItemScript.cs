3fdead295122d0355018bcbce39e6d09e344ef1c
HouseMD <vpetyushka@gmail.com>
2018-10-09 11:45:20 +0300

    Add directory separator to images path

--- Assets/Scripts/MenuScene/UI/SpeechItemScript.cs
+++ Assets/Scripts/MenuScene/UI/SpeechItemScript.cs
@@ -85,7 +85,7 @@ namespace ua.org.gdg.devfest
 
     private void Awake()
     {
-      LOGO_BASE_PATH = Application.persistentDataPath + "Graphics/";
+      LOGO_BASE_PATH = Application.persistentDataPath + Path.DirectorySeparatorChar + "Graphics/";
     }
 
     //---------------------------------------------------------------------
